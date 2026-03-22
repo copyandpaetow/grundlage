0955955c2181866e83f14425eb7927be733caa20
Lucas Paetow <lucas.paetow@gmail.com>
2026-03-22 19:38:02 +0100

    chore: fix more broken files

--- website/src/components/navbar.ts
+++ website/src/components/navbar.ts
@@ -9,6 +9,7 @@ customElements.define(
 				<a href="/grundlage/pages/async/">async</a>
 				<a href="/grundlage/pages/animation/">animation</a>
 				<a href="/grundlage/pages/list/">list</a>
+				<a href="/grundlage/pages/cubes/">cubes</a>
 			</nav>
 		`;
 	}),
