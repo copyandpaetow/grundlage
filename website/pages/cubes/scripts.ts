1126082cc64fe230c84dfafe938c538820486243
Lucas Paetow <lucas.paetow@gmail.com>
2026-04-07 20:43:17 +0200

    chore: add requirements

--- website/pages/cubes/scripts.ts
+++ website/pages/cubes/scripts.ts
@@ -4,3 +4,14 @@ import "../../src/components/navbar";
 import "./cube-scene";
 import "./cube-block";
 import "./cube-grid";
+
+/*
+What we need for an editor
+- adding different kinds of cubes (cube, pyramid, slop/prism, celinder, sphere )
+- scene grid options: height/width/depth, units, sizes
+- scene rotations
+
+- drag and drop
+- eraser, pointer
+
+*/
